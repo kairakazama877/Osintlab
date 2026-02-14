4e9a0e7fd59a81e89e6df6619a2c722674775d2b
Junkazama87 <64848168+kairakazama877@users.noreply.github.com>
2026-02-15 06:08:53 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+    BASE_DIR = "runtime"
+    RESULTS_DIR = f"{BASE_DIR}/results"
+    LOGS_DIR = f"{BASE_DIR}/logs"
+
+    RUNTIME_FOLDERS = [
+        BASE_DIR,
+        RESULTS_DIR,
+        LOGS_DIR
+    ]
+
